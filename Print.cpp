be42a207450a401844258fa9d2e3188575ccd5b0
Hodaya Marciano <hodayamar2000@gmail.com>
2019-01-02 08:32:48 -0800

    print by name and by id

--- Print.cpp
+++ Print.cpp
@@ -5,18 +5,30 @@ Print::~Print()
     std::cout << "im in Print dtor!!!" << std::endl;
 }
 
+void printSeq(IDNAp printedDna)
+{
+    int size = printedDna->getLength();
+    std::cout << "[" << printedDna->getIdSeq() << "] " << printedDna->getNameSeq() << ": ";
+    for(int i = 0; i<size; ++i)
+    {
+        std::cout << (*printedDna)[i];
+    }
+    std::cout << std::endl;
+}
+
 void Print::run(int argc, char **argv, memoryCtrl & memctrl) {
 
     char * tmp = argv[1];
+
     if (tmp[0] == '#')
     {
 
         IDNAp newDna = memctrl.getDnaSeq(atoi(++tmp), "");
-        std::cout << "[ " << newDna->getIdSeq() << " ] " << newDna->getNameSeq() << std::endl;
+        printSeq(newDna);
     }
     else if (tmp[0] == '@')
     {
-        IDNAp newDna = memctrl.getDnaSeq(-1, tmp);
-        std::cout << "[ " << newDna->getIdSeq() << " ] " << newDna->getNameSeq() << std::endl;
+        IDNAp newDna = memctrl.getDnaSeq(-1, ++tmp);
+        printSeq(newDna);
     }
 }
