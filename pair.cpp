efdfa8703b825f8b8b54d2759abfc48eaee78454
Hodaya Marciano <hodayamar2000@gmail.com>
2019-01-11 01:26:26 -0800

    Some validations

--- pair.cpp
+++ pair.cpp
@@ -5,6 +5,30 @@ pair::~pair()
     std::cout << "im in pair dtor" << std::endl;
 }
 
+bool WrongNumOfElement(int argc)
+{
+    if(argc < 1 || argc > 2)
+    {
+        std::cout << "Wrong number of argument" << std::endl;
+        return true;
+    }
+
+    return false;
+}
+
+bool WrongIdentifier(char * identifier)
+{
+    if ( (identifier[0] != '@') & (identifier[0] != '#') )
+    {
+        std::cout << "In order to find a seq identifier is needed" << std::endl;
+        return true;
+    }
+
+    return false;
+}
+
+
+
 IDNAp getSeq(char * args, memoryCtrl & m_memoryCtrl)
 {
     IDNAp dna;
@@ -26,14 +50,36 @@ IDNAp getSeq(char * args, memoryCtrl & m_memoryCtrl)
 
 void pair::run(int argc, char ** argv, memoryCtrl & m_memoryCtrl)
 {
-
-
+    std::string name;
+    char * identifier;
     IDNAp dna;
-    dna = getSeq(argv[1], m_memoryCtrl);
+
+    if( WrongNumOfElement ( argc ) )
+        return;
+
+    identifier = argv[1];
+
+    if ( WrongIdentifier ( identifier ) )
+        return;;
+        
+    dna = getSeq(identifier, m_memoryCtrl);
+
+    switch (argc)
+    {
+        case 1:
+        {
+
+            name = "";
+            break;
+        }
+        case 2:
+        {
+            name = argv[2];;
+            break;
+        }
+    }
 
     //TODO - if def name is needed. -- need to be done!
-    IDNAp pairSeq(new pairDna (dna, ""));
-
+    IDNAp pairSeq(new pairDna (dna, name));
     m_memoryCtrl.addDnaSeq(pairSeq->getIdSeq(), pairSeq->getNameSeq(), pairSeq );
-
 }
