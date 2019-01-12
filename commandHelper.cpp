4f6c4cc534a8f42d36c2efbe77529fa29bb85319
Hodaya Marciano <hodayamar2000@gmail.com>
2019-01-12 13:00:25 -0800

    Command helper is added

--- commandHelper.cpp
+++ commandHelper.cpp
@@ -1,6 +1,44 @@
-////
-//// Created by hodaya on 1/12/19.
-////
-//
-//#include "commandHelper.h"
-////
+#include "commandHelper.h"
+
+
+bool commandHelper::numOfElements(int argc, int minNumOfElements, int maxNumOfElements)
+{
+    if(argc < minNumOfElements || argc > maxNumOfElements)
+    {
+        std::cout << "Wrong number of argument" << std::endl;
+        return true;
+    }
+
+    return false;
+}
+
+
+bool commandHelper::wrongIdentifier(char * identifier)
+{
+    if ( (identifier[0] != '@') & (identifier[0] != '#') )
+    {
+        std::cout << "In order to find a seq identifier is needed" << std::endl;
+        return true;
+    }
+
+    return false;
+}
+
+IDNAp commandHelper::getSeq(char * args, memoryCtrl & m_memoryCtrl)
+{
+    IDNAp dna;
+
+    if(args[0] == '@')
+
+        dna = m_memoryCtrl.getDnaSeq(-1, ++args);
+
+    else
+    {
+        int dnaId;
+        dnaId = (atoi(++args));
+        dna = m_memoryCtrl.getDnaSeq(dnaId, "");
+    }
+
+    return dna;
+}
+
--- commandHelper.h
+++ commandHelper.h
@@ -1,16 +1,25 @@
-//#ifndef EXCELLENTEAM_ELLA_C_DNA_HODAYAMAR_COMMANDHELPER_H
-//#define EXCELLENTEAM_ELLA_C_DNA_HODAYAMAR_COMMANDHELPER_H
-//
-//class commandHelper
-//{
-//public:
-//
-//    virtual bool numOfElement( int );
-//    virtual bool wrongIdentifier( char );
-//    void setToDefualtName();
-//
-////    void printSeq()const;
-//
-//};
-//
-//#endif //EXCELLENTEAM_ELLA_C_DNA_HODAYAMAR_COMMANDHELPER_H
+#ifndef EXCELLENTEAM_ELLA_C_DNA_HODAYAMAR_COMMANDHELPER_H
+#define EXCELLENTEAM_ELLA_C_DNA_HODAYAMAR_COMMANDHELPER_H
+
+#include <cstdlib>
+#include <string>
+#include "sharedPtr.h"
+#include "MemoryCtrl.h"
+#include "Dna.h"
+#include "Icommand.h"
+
+typedef sharedPtr<Idna> IDNAp;
+
+class commandHelper : public Icommand
+{
+public:
+
+    bool numOfElements( int, int, int );
+    bool wrongIdentifier( char * );
+    IDNAp getSeq( char *, memoryCtrl & );
+
+//    void printSeq()const;
+
+};
+
+#endif //EXCELLENTEAM_ELLA_C_DNA_HODAYAMAR_COMMANDHELPER_H
